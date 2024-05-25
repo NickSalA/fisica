d0d761d616e18d42df7f73fdd87c4c8676803de0
Nick <nicksalcedo717@gmail.com>
2024-05-25 10:27:42 -0500

    8mo Commit: Ayuda Edu D:

--- Circuitos.py
+++ Circuitos.py
@@ -86,7 +86,7 @@ def resultadosRLC(voltaje, resistenciaTotal, XL, XC,Z, Zgrados, corriente, corri
     print(f'La corriente total es: {corriente:.5f} A con un desfase de {corrienteGrados:.5f} grados')
     print(f'El voltaje total es: {voltaje:.5f} V con un desfase de 0 grados\n\n')
 
-def graficarCircuito(resistencias, voltaje, serie, rlc=False, XL=None, XC=None):
+def graficarCircuito(voltaje, resistencias, serie, rlc=False, XL=None, XC=None):
     fig, ax = plt.subplots()
 
     ax.plot([0, 0], [0, 1], 'k-')  # Línea izquierda
@@ -96,8 +96,8 @@ def graficarCircuito(resistencias, voltaje, serie, rlc=False, XL=None, XC=None):
 
     componentes = resistencias
     if rlc:
-        componentes += [XL, XC]
-        nombres = ['R' + str(i + 1) for i in range(len(resistencias))] + ['XL', 'XC']
+        componentes = [resistencias[0]] + [XL, XC]
+        nombres = ['R'] + ['XL', 'XC']
     else:
         nombres = ['R' + str(i + 1) for i in range(len(resistencias))]
 
@@ -160,12 +160,12 @@ def main():
                     print(f'Circuito RLC en serie\n')
                     voltaje, resistenciaTotal, XL, XC,Z, Zgrados, corriente, corrienteGrados = resistenciaRLC(serieRLC=True)
                     resultadosRLC(voltaje, resistenciaTotal, XL, XC, Z, Zgrados, corriente, corrienteGrados, serieRLC=True)
-                    graficarCircuito(resistencias, voltaje, serie=True, rlc=True, XL=XL, XC=XC)
+                    graficarCircuito(voltaje, resistencias, serie=True, rlc=True, XL=XL, XC=XC)
                 elif opcionCircuito == 2:
                     print(f'Circuito RLC en paralelo\n')
                     voltaje, resistenciaTotal, XL, XC, Z, Zgrados, corriente, corrienteGrados = resistenciaRLC(serieRLC=False)
                     resultadosRLC(voltaje, resistenciaTotal, XL, XC, Z, Zgrados, corriente, corrienteGrados, serieRLC=False)
-                    graficarCircuito(resistencias, voltaje, serie=False, rlc=False, XL=XL, XC=XC)
+                    graficarCircuito(voltaje, resistencias, serie=False, rlc=False, XL=XL, XC=XC)
                 elif opcionCircuito == 3:
                     break
         elif opcion == 3:
